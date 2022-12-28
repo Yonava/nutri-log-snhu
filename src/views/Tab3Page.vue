936226bdedfd0ca384a4fe3fdd294cbfbb4d57ad
Yona Voss-Andreae <yona@Yonas-MacBook-Pro.local>
2022-12-28 16:46:10 -0700

    ion-select

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -1,28 +1,57 @@
 <template>
   <ion-page>
-    <video ref="video" autoplay="true" style="width: 100vw; height: 100vh" />
+    <video 
+      ref="video" 
+      autoplay 
+    />
+    <ion-select 
+      style="position: absolute; top: 0; left: 0; width: 100%;"
+      v-model="selectedCamera"
+      @ionChange="switchCamera"
+    >
+      <ion-select-option 
+        v-for="camera in cameras" 
+        :key="camera.deviceId" 
+        :value="camera"
+        >{{ camera.label }}</ion-select-option>
+    </ion-select>
   </ion-page>
 </template>
 
 <script>
-import { IonPage } from '@ionic/vue';
+import { IonPage, IonSelect, IonSelectOption } from '@ionic/vue';
 
 export default {
   data() {
     return {
-      stream: null
+      stream: null,
+      cameras: [],
+      selectedCamera: null
     }
   },
-  components: { IonPage },
+  components: { IonPage, IonSelect, IonSelectOption },
   mounted() {
-    navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-      this.stream = stream;
-      this.$refs.video.srcObject = stream;
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+      this.cameras = devices.filter(device => device.kind === 'videoinput');
+      this.selectedCamera = this.cameras[0];
+      this.startCamera();
     });
   },
   beforeUnmount() {
     if (this.stream) {
       this.stream.getTracks().forEach(track => track.stop());
+    }
+  },
+  methods: {
+    startCamera() {
+      navigator.mediaDevices.getUserMedia({ video: { deviceId: this.selectedCamera.deviceId } }).then(stream => {
+        this.stream = stream;
+        this.$refs.video.srcObject = stream;
+      });
+    },
+    switchCamera() {
+      this.stream.getTracks().forEach(track => track.stop());
+      this.startCamera();
     }
   }
 }
