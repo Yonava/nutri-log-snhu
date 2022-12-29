c001583be1b3556461dbed43d2a596b6668e5aac
Yona Voss-Andreae <yona@Yonas-MacBook-Pro.local>
2022-12-29 00:25:21 -0700

    added components

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -10,7 +10,8 @@
         <ion-toolbar>
           <ion-title size="large">Hi, Yona</ion-title>
         </ion-toolbar>
-        
+        <ion-button expand="block" @click="presentToast">Toast It!</ion-button>
+        <input type="text" v-model="toastText">
       </ion-header>
     </ion-content>
   </ion-page>
@@ -18,10 +19,26 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { toastController } from '@ionic/vue';
+import { ref } from 'vue';
 
 export default  defineComponent({
   name: 'Tab1Page',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton },
+  setup() {
+    const toastText = ref('I am a piece of toast');
+    return {
+      toastText,
+      async presentToast() {
+        const toast = await toastController.create({
+          message: toastText.value,
+          duration: 3000,
+          position: 'bottom',
+        });
+        toast.present();
+      }
+    }
+  }
 });
 </script>
