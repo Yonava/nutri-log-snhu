4ac76a9fd6d321e1ae057e64d7590e420ced28f1
yonava <yonavossandreae@gmail.com>
2023-01-23 03:36:24 -0500

    removed unused styles

--- src/components/Log/LogItemEdit.vue
+++ src/components/Log/LogItemEdit.vue
@@ -47,26 +47,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-
-/* 1. declare transition */
-.fade-move,
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
-}
-
-/* 2. declare enter from and leave to state */
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-  transform: scaleY(0.01) translate(30px, 0);
-}
-
-/* 3. ensure leaving items are taken out of layout flow so that moving
-      animations can be calculated correctly. */
-.fade-leave-active {
-  position: absolute;
-}
-</style>
