f0ee825f36ab51f00f102938071095d3dcdf5d99
yonava <yonavossandreae@gmail.com>
2023-01-23 00:31:22 -0500

    added positional shift for remove button

--- src/views/LogTab.vue
+++ src/views/LogTab.vue
@@ -5,11 +5,14 @@
         <ion-buttons slot="end">
           <ion-button>
             add
-            <ion-icon slot="start" :icon="add"></ion-icon>
+            <ion-icon slot="end" :icon="add"></ion-icon>
           </ion-button>
         </ion-buttons>
         <ion-title>Log</ion-title>
-        <ion-buttons slot="start">
+        <ion-buttons 
+          @click="removeItemsState = !removeItemsState"
+          slot="start"
+        >
           <ion-button>
             remove
             <ion-icon slot="start" :icon="remove"></ion-icon>
@@ -26,21 +29,28 @@
       <div
         v-for="(i) in items" 
         :key="i.name"
-        :style="i.day ? { position: 'sticky', top: 0, zIndex: 2 } : {}"
+        :style="i.month ? { position: 'sticky', top: 0, zIndex: 2 } : {}"
         class="parent-container"
       >
         <ion-item
           v-if="i.name"
           :router-link="{ path: `/tabs/log/edit/${i.name}` }"
+          :class="removeItemsState ? '' : 'push-item'"
+          style="transition: all 0.2s ease-in-out; width: 113%"
           button
         >
+          <ion-icon
+            :icon="remove"
+            slot="start"
+            style="color: red"
+          ></ion-icon>
           <div class="item-parent">
-            <p style="margin: 0; font-size: 9pt">
+            <p style="margin: 0; font-size: 8pt">
               {{ timeStamp.toLocaleTimeString([], { timeStyle: 'short' }) }} |
               {{ i.calories }} cals |
-              {{ Math.floor(Math.random() * 50) }}g carbs |
-              {{ Math.floor(Math.random() * 50) }}g protein |
-              {{ Math.floor(Math.random() * 50) }}g fat
+              43g carbs |
+              43g protein |
+              43g fat
             </p>
             <h2 style="text-transform: capitalize; margin: 0">{{ i.name }}</h2>
           </div>
@@ -50,7 +60,7 @@
           v-else
         >
           <h5 style="margin: 0">
-            {{ i.day }} {{ Math.floor(Math.random() * 30) + 1 }}, 2023
+            {{ i.month }} 3, 2023
           </h5>
         </div>
       </div>
@@ -90,6 +100,7 @@ export default defineComponent({
     IonIcon
   },
   setup() {
+    const removeItemsState = ref(false);
     const timeStamp = new Date();
     const items = ref([
       { name: 'apple', calories: 100 },
@@ -97,7 +108,7 @@ export default defineComponent({
       { name: 'orange', calories: 300 },
       { name: 'grape', calories: 400 },
       { name: 'pear', calories: 500 },
-      { day: 'wed' },
+      { month: 'may' },
       { name: 'pineapple', calories: 600 },
       { name: 'mango', calories: 700 },
       { name: 'watermelon', calories: 800 },
@@ -107,64 +118,36 @@ export default defineComponent({
       { name: 'blackberry', calories: 1200 },
       { name: 'kiwi', calories: 1300 },
       { name: 'lemon', calories: 1400 },
-      { day: 'tues' },
       { name: 'lime', calories: 1500 },
-      { name: 'cherry', calories: 1600 },
-      { name: 'peach', calories: 1700 },
-      { name: 'plum', calories: 1800 },
-      { name: 'apricot', calories: 1900 },
-      { name: 'nectarine', calories: 2000 },
-      { name: 'pomegranate', calories: 2100 },
-      { name: 'coconut', calories: 2200 },
-      { name: 'avocado', calories: 2300 },
-      { name: 'tomato', calories: 2400 },
-      { name: 'potato', calories: 2500 },
-      { name: 'carrot', calories: 2600 },
-      { name: 'broccoli', calories: 2700 },
-      { name: 'cauliflower', calories: 2800 },
-      { name: 'celery', calories: 2900 },
-      { day: 'thur' },
-      { name: 'spinach', calories: 3000 },
-      { name: 'lettuce', calories: 3100 },
-      { name: 'cucumber', calories: 3200 },
-      { name: 'onion', calories: 3300 },
-      { name: 'garlic', calories: 3400 },
-      { name: 'ginger', calories: 3500 },
-      { name: 'pepper', calories: 3600 },
-      { name: 'cabbage', calories: 3700 },
-      { name: 'squash', calories: 3800 },
-      { name: 'zucchini', calories: 3900 },
-      { name: 'asparagus', calories: 4000 },
-      { name: 'mushroom', calories: 4100 },
-      { name: 'eggplant', calories: 4200 },
-      { name: 'artichoke', calories: 4300 },
-      { name: 'sweet potato', calories: 4400 },
-      { name: 'yam', calories: 4500 },
-      { name: 'corn', calories: 4600 },
-      { name: 'beet', calories: 4700 },
-      { name: 'radish', calories: 4800 },
-      { name: 'turnip', calories: 4900 },
-      { name: 'rutabaga', calories: 5000 },
-      { name: 'cantaloupe', calories: 5100 },
-      { name: 'honeydew', calories: 5200 },
-      { name: 'grapefruit', calories: 5300 },
-      { name: 'papaya', calories: 5400 },
-      { name: 'durian', calories: 5500 },
-      { name: 'jackfruit', calories: 5600 },
-      { name: 'pomelo', calories: 5700 },
-      { name: 'starfruit', calories: 5800 },
-      { day: 'fri' },
-      { name: 'tangerine', calories: 5900 },
-      { name: 'persimmon', calories: 6000 },
-      { name: 'guava', calories: 6100 },
-      { name: 'lychee', calories: 6200 }
+      { name: 'coconut', calories: 1600 },
+      { name: 'avocado', calories: 1700 },
+      { name: 'peach', calories: 1800 },
+      { name: 'plum', calories: 1900 },
+      { name: 'cherry', calories: 2000 },
+      { name: 'apricot', calories: 2100 },
+      { name: 'papaya', calories: 2200 },
+      { name: 'nectarine', calories: 2300 },
+      { name: 'persimmon', calories: 2400 },
+      { name: 'pomegranate', calories: 2500 },
+      { name: 'tangerine', calories: 2600 },
+      { name: 'cantaloupe', calories: 2700 },
+      { name: 'honeydew', calories: 2800 },
+      { name: 'guava', calories: 2900 },
+      { name: 'fig', calories: 3000 },
+      { name: 'date', calories: 3100 },
+      { name: 'durian', calories: 3200 },
+      { name: 'jackfruit', calories: 3300 },
+      { name: 'lychee', calories: 3400 },
+      { name: 'mangosteen', calories: 3500 },
+      { name: 'olive', calories: 3600 }
     ]);
     return {
       arrowForward,
       add,
       remove,
       items,
-      timeStamp
+      timeStamp,
+      removeItemsState
     }
   }
 });
@@ -180,6 +163,10 @@ export default defineComponent({
   align-items: left;
   flex-direction: column;
   padding: 5px 0px;
+}
+
+.push-item {
+  transform: translateX(-12%);
 }
 
 .date-item-parent {
