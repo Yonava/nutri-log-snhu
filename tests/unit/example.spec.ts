97c013dc5710a4d8db26ab7f9028c3a72e80d490
Joshua_Burdick <31868836+Joshua-Burdick@users.noreply.github.com>
2023-01-24 21:23:23 -0500

    Update example.spec.ts

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Tab1Page from '@/views/Tab1Page.vue'
+import HomePage from '@/views/HomePage.vue'
 
-describe('Tab1Page.vue', () => {
+describe('HomePage.vue', () => {
   it('renders tab 1 Tab1Page', () => {
-    const wrapper = mount(Tab1Page)
-    expect(wrapper.text()).toMatch('Tab 1 page')
+    const wrapper = mount(HomePage)
+    expect(wrapper.text()).toMatch('Home Page')
   })
 })
