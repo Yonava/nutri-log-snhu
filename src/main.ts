95a8bf29db8d82b9cc8df03afbca8e12f5c12674
yonava <yonavossandreae@gmail.com>
2023-01-25 23:46:54 -0500

    added vuex to app

--- src/main.ts
+++ src/main.ts
@@ -28,9 +28,11 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import store from './store'
+
 Amplify.configure(awsExports);
 
-const app = createApp(App)
+const app = createApp(App).use(store)
   .use(IonicVue)
   .use(router);
 
