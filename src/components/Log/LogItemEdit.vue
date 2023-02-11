1103e222d7769726ee9a4406ac77761b6fddb382
yonava <yonavossandreae@gmail.com>
2023-02-11 16:06:34 -0500

    nutrientEditContainer added

--- src/components/Log/LogItemEdit.vue
+++ src/components/Log/LogItemEdit.vue
@@ -1,12 +1,28 @@
 <template>
   <ion-page>
     <ion-content>
-      <default-header :title="item.name">
+      <default-header title="Modify Item">
         <template #left>
           <ion-back-button default-href="/tabs/log"></ion-back-button>
         </template>
       </default-header>
-      {{ item }}
+      <div class="ion-padding">
+        <ion-input
+          v-model="item.name"
+          @ionFocus="titleFocused = true"
+          @ionBlur="titleFocused = false"
+          type="text"
+          enterkeyhint="done"
+          :style="titleStyle"
+          class="title"
+          placeholder="Item Name"
+        ></ion-input>
+        <NutrientEditContainer
+          :value="item.calories"
+          label="calories"
+          color="var(--ion-color-primary)"
+        />
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -16,22 +32,58 @@ import {
   IonPage,
   IonBackButton,
   IonContent,
+  IonInput,
 } from '@ionic/vue';
 import { useStore } from 'vuex';
+import { computed, ref } from 'vue';
+import NutrientEditContainer from './NutrientEditContainer.vue';
 
 export default {
   components: {
     IonPage,
     IonBackButton,
     IonContent,
+    IonInput,
+    NutrientEditContainer,
   },
   setup() {
     const store = useStore();
-    const item = store.getters.selectedLogItem;
+    const itemSelected = store.getters.selectedLogItem;
+    const item = structuredClone(itemSelected);
+
+    const titleFocused = ref(false);
+
+    const titleStyle = computed(() => {
+      if (titleFocused.value) {
+        return {
+          backgroundColor: 'var(--ion-color-step-150)',
+          borderBottom: '1px solid gray'
+        };
+      }
+      return {
+        backgroundColor: 'var(--ion-color-step-100)',
+      };
+    });
 
     return {
-      item
+      titleStyle,
+      titleFocused,
+      item,
     };
   }
 }
 </script>
+
+<style scoped>
+ion-input.title {
+  font-size: 2rem;
+  font-weight: 300;
+  --padding-start: 8px;
+  border-radius: 7px 7px 0 0;
+  background-color: var(--ion-color-light);
+}
+
+ion-content {
+  --overflow: hidden;
+}
+</style>
---
+++ src/components/Log/NutrientEditContainer.vue
@@ -0,0 +1,78 @@
+<template>
+  <div 
+    class="parent center" 
+    :style="{ backgroundColor: color }"
+  >
+    <span style="font-weight: 500; margin: 3px 0px">
+      {{ label }}
+    </span>
+    <ion-input
+      :value="value"
+      :readonly="!editable"
+      :style="inputStyle"
+      @ionFocus="focused = true"
+      @ionBlur="focused = false"
+      type="number"
+      enterkeyhint="done"
+      class="macros"
+    ></ion-input>
+  </div>
+</template>
+
+<script setup>
+import { IonInput } from '@ionic/vue';
+import { ref, defineProps, computed } from 'vue';
+
+const props = defineProps({
+  value: {
+    type: Number,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: false,
+    default: 'var(--ion-color-primary)',
+  },
+  editable: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+});
+
+const focused = ref(false);
+
+const inputStyle = computed(() => {
+  if (focused.value) {
+    return {
+      backgroundColor: 'var(--ion-color-step-150)',
+    };
+  }
+  return {
+    backgroundColor: 'var(--ion-color-step-100)',
+  };
+});
+</script>
+
+<style scoped>
+.parent {
+  width: 30%; 
+  border-radius: 10px
+}
+
+ion-input.macros {
+  text-align: center;
+  font-size: 2.25rem;
+  font-weight: 200;
+  --padding-start: 8px;
+  --padding-end: 8px;
+  --padding-bottom: 8px;
+  --padding-top: 8px;
+  border-radius: 0 0 7px 7px;
+  background-color: var(--ion-color-light);
+}
+</style>
