908e6a9d06e2acdb3d95b8003fce20d016466afc
yonava <yonavossandreae@gmail.com>
2023-03-03 22:16:46 -0500

    quick log perdiction rankings

--- src/utils/Log.ts
+++ src/utils/Log.ts
@@ -1,6 +1,6 @@
 import { LoggedItem } from '@/types/Log';
 
-type ScoreMapItem = {
+type ScoreItem = {
   name: string,
   score: number
 }
@@ -8,13 +8,24 @@ type ScoreMapItem = {
 export function getQuickLog(log: LoggedItem[]) {
   const scoreMap = new Map<string, number>()
 
-  // calculate frequency
   log.forEach(item => {
-    const score = scoreMap.get(item.name) ?? 0
-    scoreMap.set(item.name, score + 1)
+    const score = scoreMap.get(item.name) ?? 0;
+    let itemValue = 100;
+
+    const daysSinceLogged = Math.floor(
+      (Date.now() - new Date(item.dateAdded).getTime()) / (1000 * 60 * 60 * 24)
+    );
+    itemValue -= daysSinceLogged;
+
+    const hourDistance = Math.abs(
+      new Date().getHours() - new Date(item.dateAdded).getHours()
+    );
+    itemValue -= hourDistance * 2;
+
+    scoreMap.set(item.name, Math.max(score, score + itemValue))
   });
 
-  const scoreArray: ScoreMapItem[] = []
+  const scoreArray: ScoreItem[] = []
   scoreMap.forEach((score, name) => {
     scoreArray.push({ name, score })
   })
@@ -22,8 +33,9 @@ export function getQuickLog(log: LoggedItem[]) {
   scoreArray.sort((a, b) => b.score - a.score)
   const output = scoreArray.map(i => i.name)
 
-  if (output.length > 15) {
-    return output.slice(0, 15)
+  const maxItems = 15;
+  if (output.length > maxItems) {
+    return output.slice(0, maxItems)
   } else{
     return output
   }
