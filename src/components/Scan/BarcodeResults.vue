be7647d5bee367ea1af4eeac239b07b632c1e9af
yonava <yonavossandreae@gmail.com>
2023-03-04 22:55:10 -0500

    scan working with info

--- src/components/Scan/BarcodeResults.vue
+++ src/components/Scan/BarcodeResults.vue
@@ -8,13 +8,21 @@
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <div class="center">
+      <div class="center ion-padding">
         <ion-button 
           @click="$router.push({ name: 'Scan' })"
           expand="block"
         >
           Back To Scan
         </ion-button>
+        <img :src="relevantData.image" />
+        <code style="width: 90%">
+          {{ relevantData.nutrition }}
+          ______________________________________
+          {{ relevantData.ingredients }}
+          ______________________________________
+          {{ relevantData.name }}
+        </code>
       </div>
     </ion-content>     
   </ion-page>  
@@ -30,4 +38,31 @@ import {
   IonButton,
   IonProgressBar,
 } from '@ionic/vue';
+
+import { ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+const relevantData = ref<any>({});
+
+watch(() => route.query.barcodeContent, () => {
+  getBarcodeData();
+}, { immediate: true })
+
+async function getBarcodeData() {
+  const content = route.query.barcodeContent;
+  console.log(content);
+  const res = await fetch(`https://world.openfoodfacts.org/api/v0/product/${content}.json`);
+  const data = await res.json();
+  relevantData.value = {};
+  if (data.status === 0) {
+    relevantData.value.error = 'No food product found';
+  } else {
+    relevantData.value.nutrition = data.product.nutriments ?? null;
+    relevantData.value.image = data.product.selected_images.front.display.en ?? null;
+    relevantData.value.ingredients = data.product.ingredients_text_en ?? null; 
+    relevantData.value.name = data.product.product_name_en ?? null;
+  }
+}
 </script>
--- src/views/ScanTab.vue
+++ src/views/ScanTab.vue
@@ -1,50 +1,29 @@
 <template>
   <ion-page>
-    <ion-content class="scan-content">
-      <!-- <div>
-        <img 
-          :src="imageFrame" 
-          style="width: 100px; height: 150px; position: fixed; z-index: 99; top: 0; right: 0; border: 1px solid red; object-fit: cover;"
-        />
-        <div id="camera-feedback"></div>
-      </div> -->
-    </ion-content>
+    <ion-content class="scan-content"></ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { 
-  IonPage, 
-  IonHeader, 
-  IonToolbar, 
-  IonTitle, 
+  IonPage,
   IonContent,
   IonButton,
-  IonProgressBar,
 } from '@ionic/vue';
-import { ref, onMounted, onUnmounted, watch } from 'vue';
+import { onMounted, watch } from 'vue';
 // import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 // import { CameraPreview } from '@capacitor-community/camera-preview';
 import { useRoute, useRouter } from 'vue-router';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-import { star } from 'ionicons/icons';
 
 const route = useRoute();
 const router = useRouter();
 
-// const imageFrame = ref<string | undefined>(undefined);
-// const stillFrame = ref<string | undefined>(undefined);
-// const capture = ref<any>(undefined);
-
 watch(() => route.path, async (newVal: string, oldVal: string) => {
-  if (newVal.includes('scan')) {
-    document.body.style.background = 'transparent';
-    setTimeout(() => {
-      startScan();
-    }, 1000);
-  } else if (oldVal.includes('scan')) {
-    document.body.style.background = '';
-    stopScan();
+  if (newVal === '/tabs/scan') {
+    startScan();
+  } else if (oldVal === '/tabs/scan') {
+    await BarcodeScanner.stopScan();
   }
 });
 
@@ -53,43 +32,14 @@ onMounted(async () => {
   startScan();
 });
 
-// async function startCameraPreview() {
-//   console.log('startCameraPreview')
-//   await CameraPreview.start({
-//     parent: "camera-feedback",
-//     position: "rear",
-//     width: innerWidth,
-//     height: innerHeight - 52,
-//     disableAudio: true,
-//     toBack: true,
-//   });
-//   capture.value = setInterval(async () => {
-//     const image = await CameraPreview.capture({ 
-//       quality: 2,
-//     });
-//     imageFrame.value = 'data:image/jpeg;base64,' + image.value;
-//   }, 3000);
-// }
-
-// async function stopCameraPreview() {
-//   clearInterval(capture.value);
-//   await CameraPreview.stop();
-// }
-
 async function startScan() {
   const result = await BarcodeScanner.startScan();
-  console.log('Barcode data', result);
   router.push({
     name: 'BarcodeResults',
     query: {
       barcodeContent: result.content,
     },
   });
-  stopScan();
-}
-
-async function stopScan() {
-  await BarcodeScanner.stopScan();
 }
 </script>
 
