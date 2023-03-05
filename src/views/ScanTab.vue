18ba3c8d6c02aeb896ccbdf150ff07ab5b8859a1
yonava <yonavossandreae@gmail.com>
2023-03-05 02:14:15 -0500

    error handling on scan modal

--- src/views/ScanTab.vue
+++ src/views/ScanTab.vue
@@ -9,35 +9,85 @@
         :breakpoints="[0, 0.95]"
       >
         <ion-content class="ion-padding">
-          <div class="center">
+          <div 
+            v-if="!relevantData.error"
+            class="center"
+          >
             <div style="display: flex; flex-direction: row;">
               <img 
+                v-if="relevantData.image"
                 :src="relevantData.image" 
                 style="width: 100px; height: 100px; border-radius: 10px; object-fit: cover; margin-right: 10px;"
               />
-              <h1 style="margin: 0; font-weight: 700;">
+              <h1 
+                v-if="relevantData.name"
+                style="margin: 0; font-weight: 700;"
+              >
                 {{ relevantData.name }}
+              </h1>
+              <h1
+                v-else
+                style="margin: 0; font-weight: 700;"
+              >
+                No name available for this product
               </h1>
             </div>
             <div style="width: 100%; background: rgba(255, 255, 255, 0.1); margin-top: 15px; border-radius: 5px; padding: 10px">
-              <div style="font-weight: bold; font-size: 2rem">
+              <div style="font-weight: bold; font-size: 1.5rem">
                 Nutrition Facts
               </div>
-              <div style="margin-top: 5px;">
-                {{ relevantData.nutrition.fat_serving }}g Fat
-                {{ relevantData.nutrition.carbs_serving }}g Carbs
-                {{ relevantData.nutrition.protein_serving }}g Protein
-                {{ relevantData.nutrition.calcium_serving }}g Calcium
-                {{ relevantData.nutrition.iron_serving * 1000 }}mg Iron
+              <div 
+                v-if="relevantData.nutrition['energy-kcal_serving']"
+                style="margin-top: 5px;"
+              >
+                {{ relevantData.nutrition['energy-kcal_serving'] }} Calories -
+                {{ Math.round(relevantData.nutrition.proteins_serving) }}g Protein -
+                {{ Math.round(relevantData.nutrition.fat_serving) }}g Fat -
+                 {{ Math.round(relevantData.nutrition['saturated-fat_serving']) }}g Saturated Fat - 
+                {{ Math.round(relevantData.nutrition['trans-fat_serving']) }}g Trans Fat -
+                {{ Math.round(relevantData.nutrition.carbohydrates_serving) }}g Carbs - 
+                {{ Math.round(relevantData.nutrition.sugars_serving) }}g Sugars -
+                {{ Math.round(relevantData.nutrition.fiber_serving) }}g Fiber -
+                {{ Math.round(relevantData.nutrition.calcium_serving * 1000) }}mg Calcium - 
+                {{ Math.round(relevantData.nutrition.iron_serving * 1000) }}mg Iron -
+                {{ Math.round(relevantData.nutrition.sodium_serving * 1000) }}mg Sodium -
+                {{ Math.round(relevantData.nutrition.cholesterol_serving * 1000) }}mg Cholesterol
+              </div>
+              <div v-else>
+                No nutrition facts available
               </div>
             </div>
-            <code style="width: 90%">
-              {{ relevantData.nutrition }}
-              <!-- ______________________________________
-              {{ relevantData.ingredients }}
-              ______________________________________
-              {{ relevantData.name }} -->
-            </code>
+            <div style="width: 100%; max-height: 225px; overflow-y: auto; background: rgba(255, 255, 255, 0.175); margin-top: 15px; border-radius: 5px; padding: 10px">
+              <div style="font-weight: bold; font-size: 1.5rem">
+                Ingredients
+              </div>
+              <div 
+                v-if="relevantData.ingredients"
+                style="margin-top: 5px; font-size: 1rem"
+              >
+                {{ relevantData.ingredients }}
+              </div>
+              <div v-else>
+                No ingredients available
+              </div> 
+            </div>
+            <ion-button
+              v-if="relevantData.name && relevantData.nutrition['energy-kcal_serving']"
+              @click="addProductToLog"
+              color="success"
+              style="position: absolute; bottom: 50px; width: 92%;"
+            >
+              <ion-icon
+                :icon="addOutline"
+                style="margin-right: 5px;"
+              ></ion-icon>
+              Add to Log
+            </ion-button>
+          </div>
+          <div v-else>
+            <h1 style="font-weight: 700; text-align: center;">
+              {{ relevantData.error }}
+            </h1>
           </div>
         </ion-content>
       </ion-modal>
@@ -54,15 +104,19 @@ import {
   alertController,
   IonIcon
 } from '@ionic/vue';
-import { closeOutline } from 'ionicons/icons';
+import { closeOutline, addOutline } from 'ionicons/icons';
 import { onMounted, watch, ref } from 'vue';
 // import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { CameraPreview } from '@capacitor-community/camera-preview';
 import { useRoute, useRouter } from 'vue-router';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { useStore } from 'vuex';
+import { LoggedItem } from '@/types/Log';
 
 const route = useRoute();
 const router = useRouter();
+const store = useStore();
+
 const barcodeContent = ref<string>('');
 const relevantData = ref<any>({});
 const productModal = ref<any>();
@@ -117,13 +171,40 @@ async function getBarcodeData() {
   const data = await res.json();
   relevantData.value = {};
   if (data.status === 0) {
-    relevantData.value.error = 'No food product found';
+    relevantData.value.error = 'We Apologize, But This Item Is Not In Our Database';
   } else {
     relevantData.value.nutrition = data.product.nutriments ?? null;
     relevantData.value.image = data.product.selected_images.front.display.en ?? null;
     relevantData.value.ingredients = data.product.ingredients_text_en ?? null; 
     relevantData.value.name = data.product.product_name_en ?? null;
   }
+}
+
+function addProductToLog() {
+  store.dispatch('postLoggedItem', {
+    name: relevantData.value.name,
+    calories: relevantData.value.nutrition['energy-kcal_serving'],
+    macro: {
+      carbohydrates: {
+        total: Math.round(relevantData.value.nutrition.carbohydrates_serving),
+        added_sugars: 0,
+        sugars: Math.round(relevantData.value.nutrition.sugars_serving),
+      },
+      fat: {
+        total: Math.round(relevantData.value.nutrition.fat_serving),
+        saturated: Math.round(relevantData.value.nutrition['saturated-fat_serving']),
+        trans: Math.round(relevantData.value.nutrition['trans-fat_serving'])
+      },
+      protein: Math.round(relevantData.value.nutrition.proteins_serving),
+      fiber: Math.round(relevantData.value.nutrition.fiber_serving),
+      sodium: Math.round(relevantData.value.nutrition.sodium_serving * 1000),
+      cholesterol: Math.round(relevantData.value.nutrition.cholesterol_serving * 1000),
+      calcium: Math.round(relevantData.value.nutrition.calcium_serving * 1000),
+      potassium: 0,
+      iron: Math.round(relevantData.value.nutrition.iron_serving * 1000),
+    }
+  });
+  closeModal();
 }
 </script>
 
